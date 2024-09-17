0835bdbfd874ba52a0fe448f1f039ea10af03bc7
shahd Hesham Ibrahim <shahedhesham13@gmail.com>
2024-09-17 11:15:54 +0300

    [1251] [Time Beats: 66.66%] - LeetPush

---
+++ Basic_Aggregate_Functions/1251-Average-Selling-Price.sql
@@ -0,0 +1,10 @@
+-- Write your PostgreSQL query statement below
+SELECT p.product_id,
+       ROUND(
+           COALESCE(SUM(CAST(p.price AS DECIMAL) * u.units) / NULLIF(SUM(u.units), 0), 0)
+       , 2) AS average_price
+FROM Prices p
+LEFT JOIN UnitsSold u
+ON p.product_id = u.product_id
+AND u.purchase_date BETWEEN p.start_date AND p.end_date
+GROUP BY p.product_id;
